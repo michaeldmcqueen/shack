1ec550548372679fe55173c7411fc42d7843731c
Michael McQueen <michaeldmcqueen@gmail.com>
2017-09-15 10:24:39 -0600

    Removing Old Files

--- assets/js/prod/scripts.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){Modernizr.mq("only all")?$("html").addClass("mq"):$("html").addClass("no-mq"),$(".menu-btn").on("click touch",function(){$(".menu").toggleClass("show")})});
